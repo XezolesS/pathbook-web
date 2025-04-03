f0e95949d11b0177adca75ff85d61fd4e4e5a3bb
PlemRuckol <ruckol9609@naver.com>
2025-04-04 00:25:43 +0900

    랜딩 페이지에  각각 파트 영역 생성함 랜딩 페이지 외부 CSS 파일 생성 및 참조함
    생성된 파트 영역에 외부 .tsx를 참조하거나 해당 <div> 안에 구현하면 됨.
    
    className은 임시로 지정하였으며, 추후 조율할 것.
    
    id backgound_color_gray는 각각의 파트 영역의 시각화를 위함.

--- app/features/LandingPage.tsx
+++ app/features/LandingPage.tsx
@@ -1,10 +1,34 @@
+import './LandingPageStyle.css'
+
 export default function LandingPage() {
   return (
     <>
-      <div>
-        <p>
-          Landing Page
-        </p>
+      <div className='Landing_Main_Container'>
+        <div className='Landing_Container'>
+          <div className='Info' id='background_color_gray'>
+            Info
+          </div>
+
+          <div className='Landing_Container_Right'>
+            <div className='Login_button' id="background_color_gray">
+              Login_button
+            </div>
+            
+            <div className='Landing_Contents_Viewer' id="background_color_gray">
+              Landing_Contents_Viewer
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='Landing_Info_Container'>
+        <div className='Info_Container' id='background_color_gray'>
+          Info_Container
+        </div>
+      </div>
+
+      <div className='footer_Container'>
+        footer
       </div>
     </>
   )
