3a4f12951d18422f1d02f118d9caff9a39b152cc
XezolesS <tndid7876@gmail.com>
2025-04-06 14:20:34 +0900

    스타일 수정.

--- app/features/Register.tsx
+++ app/features/Register.tsx
@@ -20,9 +20,12 @@ export default function Register() {
     } else {
       setShowMismatch(false);
       console.log('회원가입 요청.');
-      //여기에 API 연결하여 회원가입 처리하면 됨.
+      // TODO: 여기에 API 연결하여 회원가입 처리하면 됨.
     }
   };
+
+  // TODO: 비밀번호 유효성 검사 추가
+
   return (
     <>
       <div>
@@ -33,63 +36,64 @@ export default function Register() {
           <div className='register-text'>회원가입</div>
           <form className='register-form' onSubmit={handleSubmit}>
             <label htmlFor='email'>이메일</label>
-            <br />
-            <input
-              className='input-email'
-              type='email'
-              id='email'
-              name='email'
-              placeholder='이메일을 입력하세요'
-              required
-            />
-            <br />
-            <label htmlFor='password'>비밀번호</label>
-            <br />
-            <input
-              className='input-password'
-              type='password'
-              id='password'
-              name='password'
-              placeholder='비밀번호를 입력하세요'
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className='password-guide'>
-              • 영어 대소문자 포함<br />
-              • 숫자(0-9) 포함<br />
-              • 특수문자 1자 이상 포함
+            <div className='register-form-section'>
+              <input
+                className='input input-email'
+                type='email'
+                id='email'
+                name='email'
+                placeholder='이메일을 입력하세요'
+                required
+              />
             </div>
-            <label htmlFor='confirm-password'>비밀번호 재입력</label>
-            <br />
-            <input
-              className='input-password'
-              type='password'
-              id='confirm-password'
-              name='confirmPassword'
-              placeholder='비밀번호를 다시 입력하세요'
-              required
-              ref={confirmRef}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            {showMismatch && (
-              <div className='password-mismatch'>
-                입력한 비밀번호가 다릅니다.
+            <div className='register-form-section'>
+              <label htmlFor='password'>비밀번호</label>
+              <input
+                className='input input-password'
+                type='password'
+                id='password'
+                name='password'
+                placeholder='비밀번호를 입력하세요'
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className='password-guide'>
+                • 영어 대소문자 포함<br />
+                • 숫자(0-9) 포함<br />
+                • 특수문자 1자 이상 포함
               </div>
-            )}
-            <br />
-            <label htmlFor='name'>닉네임</label>
-            <br />
-            <input
-              className='input-email'
-              type='text'
-              id='name'
-              name='name'
-              placeholder='닉네임을 입력하세요'
-              required
-            />
-            <br />
+            </div>
+            <div className='register-form-section'>
+              <label htmlFor='confirm-password'>비밀번호 재입력</label>
+              <input
+                className='input input-confirm-password'
+                type='password'
+                id='confirm-password'
+                name='confirmPassword'
+                placeholder='비밀번호를 다시 입력하세요'
+                required
+                ref={confirmRef}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              {showMismatch && (
+                <div className='password-mismatch'>
+                  입력한 비밀번호가 다릅니다.
+                </div>
+              )}
+            </div>
+            <div className='register-form-section'>
+              <label htmlFor='name'>닉네임</label>
+              <input
+                className='input input-nickname'
+                type='text'
+                id='name'
+                name='name'
+                placeholder='닉네임을 입력하세요'
+                required
+              />
+            </div>
             <button type='submit' className='register-form-submit'>
               회원가입
             </button>
