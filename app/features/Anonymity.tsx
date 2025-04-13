f6e3141957f3ddcbe6dbd08aaca8acae606ef33c
PlemRuckol <PlemRuckol9609@gmail.com>
2025-04-13 21:53:49 +0900

    링크 가능한 버튼들에 대한 링크 지정.
    사소한 css 수정

--- app/features/Anonymity.tsx
+++ app/features/Anonymity.tsx
@@ -2,8 +2,8 @@ export default function Anonymity() {
     return (
       <>
         <div className='anonymity-comment'>익명으로 둘러보는 중입니다.</div>
-          <div className='login-button'><span>로그인</span></div>
-          <div className='register-button'><span>회원가입</span></div>
+          <a href="login"><div className='login-button'><span>로그인</span></div></a>
+          <a href="register"><div className='register-button'><span>회원가입</span></div></a>
       </>
     )
   }
