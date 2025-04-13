5f10e5267b895deb01f9e8392b34d21b1cb61451
channhuii <36101844+PlemRuckol@users.noreply.github.com>
2025-04-13 22:40:54 +0900

    익명 둘러보기 컴포넌트

--- app/features/Anonymity.tsx
+++ app/features/Anonymity.tsx
@@ -1,6 +1,6 @@
 export default function Anonymity() {
     return (
-      <>
+      <> 
         <div className='anonymity-comment'>익명으로 둘러보는 중입니다.</div>
           <a href="login"><div className='login-button'><span>로그인</span></div></a>
           <a href="register"><div className='register-button'><span>회원가입</span></div></a>
