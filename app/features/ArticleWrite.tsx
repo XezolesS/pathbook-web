6dd10a1145fd2aafda34475808c7f712883da494
jungboX <stonesys123@naver.com>
2025-04-16 18:41:33 +0900

    Quill Editor 수정

--- app/features/ArticleWrite.tsx
+++ app/features/ArticleWrite.tsx
@@ -1,45 +1,7 @@
-import { useEffect, useRef } from "react";
-import 'react-quill/dist/quill.snow.css';
 import './ArticleWriteStyle.css';
+import RichTextEditor from './component/RichTextEditor';
 
 export default function ArticleWrite() {
-  const editorRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && editorRef.current) {
-      import('quill').then((QuillModule) => {
-        const Quill = QuillModule.default;
-        
-        const toolbarOptions = [
-          ['bold', 'italic', 'underline', 'strike'],
-          [{ align: [] }],
-          [{ size: ['small', false, 'large', 'huge'] }],
-          [
-            {
-              color: [
-                '#000000',
-                '#e60000',
-                '#ff9900',
-                '#ffff00',
-                '#008a00',
-                '#0066cc',
-                'custom-color',
-              ],
-            },
-            { background: [] },
-          ],
-          ['image'],
-        ];
-        
-        new Quill(editorRef.current!, {
-          modules: { toolbar: toolbarOptions },
-          placeholder: '내용을 입력해 주세요...',
-          theme: 'snow',
-        });
-      });
-    }  
-  }, []);
-
   return (
     <>
       <div className="article-write-frame">
@@ -48,7 +10,7 @@ export default function ArticleWrite() {
           <input className="subject" placeholder="제목을 입력해 주세요"></input>
           <p></p>
           <input className="tag-list" placeholder="#태그_추가"></input>
-          <div id="editor" ref={editorRef} style={{ minHeight: '200px' }}></div>
+          <RichTextEditor />
         </div>
         <div className="button-container">
           <button className="submit">작성하기</button>
