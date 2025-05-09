3856d4cbb6b735f8c5dcf51d6cda219a2ed81ce2
PlemRuckol <PlemRuckol9609@gmail.com>
2025-05-09 10:15:37 +0900

    디자인 수정 및 기능 추가

--- app/features/ArticleContents.tsx
+++ app/features/ArticleContents.tsx
@@ -1,15 +1,27 @@
 import "./ArticleContentsStyle.css";
+import { formatCountNumber } from './script/count';
 
-export default function ArticleContents() {
+interface ArticleContentsProps{
+  writerNickname: string;
+  writeTime: string;
+  title: string;
+  tagList: string;
+  description: string;
+  writerId: string;
+  chat: number;
+  like: number;
+  bookmark: number;
+}
+
+export default function ArticleContents(Details: ArticleContentsProps) {
   return (
     <>
       <div className="article-contents-frame">
         <div className="map">
-          <div className="subject">제목</div>
+          <div className="subject">{Details.title}</div>
           <div className="bottom-container">
-            <div className="tag-list">#태그 #조선대학교 #광주</div>
-            <div className="description">설명 / 광주광역시 동구 서석동에 위치한 4년제 사립 종합대학이자 최초의민립대학이다.
-              1946년 개교하였으며, 산하에 조선대학교부속고등학교, 조선대학교 ...
+            <div className="tag-list">{Details.tagList}</div>
+            <div className="description">{Details.description}
             </div>
           </div>
         </div>
@@ -17,22 +29,22 @@ export default function ArticleContents() {
           <div className="writer">
             <div className="profile-pic"></div>
             <div className="text">
-              <div className="author">작성자</div>
-              <div className="id">@user_id · 2025/01/23 12:34 PM</div>
+              <div className="author">{Details.writerNickname}</div>
+              <div className="id">{Details.writerId} · {Details.writeTime}</div>
             </div>
           </div>
           <div className="show-count">
             <div className="chats">
               <img className="chat" src=".\app\features\assets\chat.svg" />
-              <div className="chat-count">12</div>
+              <div className="chat-count">{formatCountNumber(Details.chat)}</div> 
             </div>
             <div className="likes">
               <img className="heart" src=".\app\features\assets\heart.svg" />
-              <div className="like-count">2,025</div>
+              <div className="like-count">{formatCountNumber(Details.like)}</div>
             </div>
             <div className="bookmarks">
               <img className="book-open" src=".\app\features\assets\book-open.svg" />
-              <div className="bookmark-count">2,025</div>
+              <div className="bookmark-count">{formatCountNumber(Details.bookmark)}</div>
             </div>
               
           </div>
