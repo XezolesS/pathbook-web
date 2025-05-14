ed085680f23fe09136f49a61709df7db7df8f852
XezolesS <tndid7876@gmail.com>
2025-05-14 23:41:39 +0900

    로그인 페이지 구조 변경

--- app/routes.ts
+++ app/routes.ts
@@ -2,12 +2,12 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("./pages/Landing.tsx"),
-
-  route("home", "./features/Home.tsx"),
-  route("login", "./features/Login.tsx"),
+  
+  route("login", "./pages/Login.tsx"),
   route("register", "./features/Register.tsx"),
   route("reset-password-request", "./features/ResetPasswordRequest.tsx"),
   route("reset-password", "./features/ResetPassword.tsx"),
 
   route("main", "./pages/Main.tsx"),
+  route("home", "./features/Home.tsx"),
 ] satisfies RouteConfig;
