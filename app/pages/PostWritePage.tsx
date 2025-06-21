816facf8da7e37f620d64fce263df9630b1ea479
PlemRuckol <PlemRuckol9609@gmail.com>
2025-06-21 20:43:51 +0900

    PostWritePage 수정 모드 연동

--- app/pages/PostWritePage.tsx
+++ app/pages/PostWritePage.tsx
@@ -1,12 +1,15 @@
+import { useLocation } from "react-router";
 import ArticleWrite from "../components/PostWrite.js";
+import { Post } from "../api/pathbook/types/Post";
 import "./Main.css";
 
-export default function PostMainViewPage() {
+export default function PostWritePage() {
+  const location     = useLocation();
+  const editingPost  = location.state?.post as Post | undefined;
+
   return (
-    <>
-      <div className="articlewrite-container">
-        <ArticleWrite />
-      </div>
-    </>
+    <div className="articlewrite-container">
+      <ArticleWrite editingPost={editingPost} /> 
+    </div>
   );
 }
